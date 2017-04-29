84268f878b43482ef7e507803105d30240d7a991
Dustin Graham <git@dustingraham.com>
2017-04-28 21:46:35 -0700

    Normalize items.

--- game/scripts/vscripts/app/systems/characters/abilities/self_regen.lua
+++ game/scripts/vscripts/app/systems/characters/abilities/self_regen.lua
@@ -1,14 +1,6 @@
 self_regen = self_regen or class({})
 local spell = self_regen
 
-function spell:GetBehavior()
-    return DOTA_ABILITY_BEHAVIOR_NO_TARGET +
-    DOTA_ABILITY_BEHAVIOR_CHANNELLED +
-    -- These seem ignored...
-    DOTA_ABILITY_BEHAVIOR_DONT_RESUME_ATTACK +
-    DOTA_ABILITY_BEHAVIOR_DONT_RESUME_MOVEMENT
-end
-
 function spell:OnAbilityPhaseStart()
     EmitSoundOn('Hero_Omniknight.GuardianAngel.Cast', self:GetCaster())
     return true
